9ec90bd1474803f82960c45539bb7c6b624a67e4
Tony <tbridges42@gmail.com>
2018-08-21 20:51:09 -0500

    Test CD with failing test

---
+++ src/test/java/us/bridgeses/MapMate/SampleTest.java
@@ -0,0 +1,17 @@
+package us.bridgeses.MapMate;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by tbrid on 8/21/2018.
+ */
+public class SampleTest {
+
+    @Test
+    public void failTest() {
+        assertEquals(0,1);
+    }
+}
